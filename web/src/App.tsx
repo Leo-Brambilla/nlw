ae447210a888f495f0d80f43751d679ca135cbae
Leo-Brambilla <leo.a.brambilla@gmail.com>
2023-03-03 19:40:19 -0300

    Front-end

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+import { Habit } from "./components/Habit"
+import './styles/global.css'
+
+function App() {
+  return (
+    <div>
+    <Habit completed={'D'}/>    
+    <Habit completed={'S'}/>
+    <Habit completed={'T'}/>
+    <Habit completed={'Q'}/>
+    <Habit completed={'Q'}/>
+    <Habit completed={'S'}/>
+
+    </div>
+    
+  )
+}
+export default App
