2ea1edd00b07dccde7bbc7fbead8add51f248973
Leo-Brambilla <leo.a.brambilla@gmail.com>
2023-03-09 17:55:19 -0300

    criação da estrutura do banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,17 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { prisma } from './lib/prisma'
+import { appRoutes } from './routes'
 
 
 const app = Fastify()
-const prisma = new PrismaClient()
+
 
 app.register(cors, {
     // origin: ['http://localhost:3333'] dessa forma apenas o endereço especificado teria acesso ao banco de dados
 })
+app.register(appRoutes)
 
-app.get('/hello', async ()  => {
-    const habits = await prisma.habit.findMany({
-        where: {
-            title: {
-                startsWith: 'Beber'
-            }
-        }
-    })
-    
-    return habits
-})
 
 app.listen({
     port:3333,
