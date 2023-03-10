eef04fadd8771077235bc6f0c6e0e81a8889edfd
Leo-Brambilla <leo.a.brambilla@gmail.com>
2023-03-10 15:24:28 -0300

    inicio da estrutura da homepage

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,39 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-date-from-year-beginning"
+import { HabitDay } from "./HabitDay"
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+const summaryDates = generateDatesFromYearBeginning()
+
+const minimumSummaryDatesSize = 18*7
+const amountOfDaysToFill = minimumSummaryDatesSize
+
+export function SummaryTable() {
+    return (
+        <div className="w-full flex ">
+            <div className="grid grid-rows-7 grid-flow-row gap-3">
+                {weekDays.map((weekDay, i) => {
+                    return (
+                        <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl h-10 w-10 flex items-center justify-center font-bold">
+                            {weekDay}
+                        </div>
+                    )
+                })}
+            </div>
+
+            <div className="grid grid-rows-7 grid-flow-col gap-3">
+                {summaryDates?.map(date => { 
+                    return <HabitDay key={date.toString()} />
+                })}
+
+                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill}).map((_, i) => {
+                    return (
+                        <div key={i} className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center opacity-40 cursor-not-allowed" />
+                    )
+                })}
+
+
+            </div>
+        </div>
+    )
+}
