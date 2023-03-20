bfdd0893a31e34875725c43dc9522dad27eb3a6e
Leo-Brambilla <leo.a.brambilla@gmail.com>
2023-03-20 12:30:56 -0300

    criação de novas telas do mobile

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -2,13 +2,16 @@ import { View, Text, ScrollView } from 'react-native';
 import { HabitDay, DAY_SIZE } from '../components/HabitDay';
 import { Header } from '../components/Header';
 import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates'
+import { useNavigation } from '@react-navigation/native'
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 const datesFromYearStart = generateRangeDatesFromYearStart();
-const minimumSummaryDatesSizes = 18*7;
+const minimumSummaryDatesSizes = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+
+    const { navigate } = useNavigation()
     return (
         <View className="flex-1 bg-background" px-8 pt-16>
             <Header />
@@ -27,33 +30,31 @@ export function Home() {
                 }
             </View>
             <ScrollView
-            showsVerticalScrollIndicator={false}
-            contentContainerStyle={{paddingBottom: 100}}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
             >
-            <View className='flex-row flex-wrap'>
-            {
-                    datesFromYearStart.map(date => (
-                        <HabitDay 
-                        key={date.toISOString()}
-                        />
-                    ))
-                }
+                <View className='flex-row flex-wrap'>
+                    {
+                        datesFromYearStart.map(date => (
+                            <HabitDay
+                                key={date.toISOString()}
+                                onPress={() => navigate('habit', { date: date.toISOString() })}
+                            />
+                        ))
+                    }
+                    {
+                        amountOfDaysToFill > 0 && Array
+                            .from({ length: amountOfDaysToFill })
+                            .map((_, index) => (
+                                <View
+                                    className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40'
+                                    style={{ width: DAY_SIZE, height: DAY_SIZE }}
+                                />
+                            ))
+                    }
+                </View>
+            </ScrollView>
 
-
-                
-                {
-                amountOfDaysToFill > 0 && Array
-                .from({ length: amountOfDaysToFill })
-                .map((_, index) => (
-                    <View
-                    className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40'
-                    style={{ width: DAY_SIZE, height: DAY_SIZE }}
-                     />
-                ))
-            }            
-            </View>
-            </ScrollView>            
-            
         </View>
     )
 
